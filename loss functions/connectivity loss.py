f39b39ac698b5498dff0dd704fcae12b6b02bdf5
AlexYouXin <48478426+AlexYouXin@users.noreply.github.com>
2023-05-26 00:18:07 +0800

    Update connectivity loss.py

--- loss functions/connectivity loss.py
+++ loss functions/connectivity loss.py
@@ -60,8 +60,7 @@ class the_connectivity_loss(nn.Module):
                 distance_matrix[torch.int(count * 0.005):] = 0
 	    index = index[indices]
 
-            # max_distance = torch.max(distance_matrix)
-            max_distance_index = torch.argmax(distance_matrix)
+	    max_distance_value, max_distance_index = torch.max(distance_matrix)
 
             # vertex
             vertex_z = index[max_distance_index][0]
