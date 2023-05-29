8cb3349a95708fd062db6cff038c30dd3bdecdcd
AlexYouXin <48478426+AlexYouXin@users.noreply.github.com>
2023-05-29 23:48:24 +0800

    Update connectivity loss.py

--- loss functions/connectivity loss.py
+++ loss functions/connectivity loss.py
@@ -11,13 +11,30 @@ import os
 from tqdm import tqdm
 from sklearn.metrics.pairwise import pairwise_distances
 from sklearn.neighbors.kde import KernelDensity
+from sklearn.utils.extmath import cartesian
 import skimage.io
 
+torch.set_default_dtype(torch.float32)
 
+def cdist(x, y):
+    '''
+    Input: x is a Nxd Tensor
+           y is a Mxd Tensor
+    Output: dist is a NxM matrix where dist[i,j] is the norm
+           between x[i,:] and y[j,:]
+    i.e. dist[i,j] = ||x[i,:]-y[j,:]||
+    '''
+    differences = x.unsqueeze(1) - y.unsqueeze(0)
+    distances = torch.sum(differences**2, -1).sqrt()
+    return distances
 
 # To localize the LA and LAA respectively
 # count: number of voxels in LAA
 def locate_LA_LAA(input):
+    '''
+    Input: 
+    Output: 
+    '''
     input = input[0]
     LAA = torch.zeros_like(input)
     LAA[input == 1] = 1
@@ -27,18 +44,58 @@ def locate_LA_LAA(input):
 
     return LAA, LA, count
 
-def centroid_calculate(LAA):
+def centroid_calculate_LAA(LAA):
 
     count = torch.count_nonzero(LAA)
-    index = torch.nonzero(LAA)
+    # index = torch.nonzero(LAA)
     # sum by coloumn directions
-    centroid = torch.sum(index, 0) / count
+    # centroid = torch.sum(index, 0) / count
+	
+    z, y, x = LAA.size()
+    index_tensor = torch.zeros((3, z, y, x))
+    for i in range(z):
+	for j in range(y):
+            for k in range(x):
+	        index_tensor[:, i, j, k] = torch.tensor((i, j, k))
     return centroid, index
 
 
 def index_LA(LA):
     index = torch.nonzero(LA)
     return index
+
+def averaged_hausdorff_distance(set1, set2, max_ahd=np.inf):
+    """
+    Compute the Averaged Hausdorff Distance function
+     between two unordered sets of points (the function is symmetric).
+     Batches are not supported, so squeeze your inputs first!
+    :param set1: Array/list where each row/element is an N-dimensional point.
+    :param set2: Array/list where each row/element is an N-dimensional point.
+    :param max_ahd: Maximum AHD possible to return if any set is empty. Default: inf.
+    :return: The Averaged Hausdorff Distance between set1 and set2.
+    """
+
+    if len(set1) == 0 or len(set2) == 0:
+        return max_ahd
+
+    set1 = np.array(set1)
+    set2 = np.array(set2)
+
+    assert set1.ndim == 2, 'got %s' % set1.ndim
+    assert set2.ndim == 2, 'got %s' % set2.ndim
+
+    assert set1.shape[1] == set2.shape[1], \
+        'The points in both sets must have the same number of dimensions, got %s and %s.'\
+        % (set2.shape[1], set2.shape[1])
+
+    d2_matrix = pairwise_distances(set1, set2, metric='euclidean')
+
+    res = np.average(np.min(d2_matrix, axis=0)) + \
+        np.average(np.min(d2_matrix, axis=1))
+
+    return res
+
+
 
 
 # input: after softmax and argmax
