7172bd20b5e6db6dd0f10e96753161245ee905c7
AlexYouXin <48478426+AlexYouXin@users.noreply.github.com>
2023-05-30 00:26:09 +0800

    Update connectivity loss.py

--- loss functions/connectivity loss.py
+++ loss functions/connectivity loss.py
@@ -87,38 +87,6 @@ def index_LA(LA):
     LA_index = del_tensor_zero_row(LA_index)
     return LA_index
 
-def averaged_hausdorff_distance(set1, set2, max_ahd=np.inf):
-    """
-    Compute the Averaged Hausdorff Distance function
-     between two unordered sets of points (the function is symmetric).
-     Batches are not supported, so squeeze your inputs first!
-    :param set1: Array/list where each row/element is an N-dimensional point.
-    :param set2: Array/list where each row/element is an N-dimensional point.
-    :param max_ahd: Maximum AHD possible to return if any set is empty. Default: inf.
-    :return: The Averaged Hausdorff Distance between set1 and set2.
-    """
-
-    if len(set1) == 0 or len(set2) == 0:
-        return max_ahd
-
-    set1 = np.array(set1)
-    set2 = np.array(set2)
-
-    assert set1.ndim == 2, 'got %s' % set1.ndim
-    assert set2.ndim == 2, 'got %s' % set2.ndim
-
-    assert set1.shape[1] == set2.shape[1], \
-        'The points in both sets must have the same number of dimensions, got %s and %s.'\
-        % (set2.shape[1], set2.shape[1])
-
-    d2_matrix = pairwise_distances(set1, set2, metric='euclidean')
-
-    res = np.average(np.min(d2_matrix, axis=0)) + \
-        np.average(np.min(d2_matrix, axis=1))
-
-    return res
-
-
 
 
 # input: after softmax and argmax
